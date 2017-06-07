21e1353d5504b021e9549e5250b61efdfa5b5b2d
Sean Parsons <seanpatrick2013@gmail.com>
2017-06-07 15:29:41 -0400

    Test the decorator return value without using emitters.

---
+++ tests/test_decorator_return_val.py
@@ -0,0 +1,18 @@
+""" Test the emitted functions return value."""
+
+from event_bus import EventBus
+
+bus = EventBus()
+EVENT_NAME = 'completed'
+
+
+@bus.on(event=EVENT_NAME)
+def on_completed():
+    return "It works!"
+
+
+def test_decorator_return_val():
+    """ Tests tha the decorator returns value. """
+    returned = on_completed()
+
+    assert returned == "It works!"
