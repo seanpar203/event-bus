6408cd8c27427680848db862d616493d9c12728a
Sean Parsons <seanpatrick2013@gmail.com>
2017-06-07 15:28:26 -0400

    Normalized constant value names in test files.

--- tests/test_event_emit.py
+++ tests/test_event_emit.py
@@ -4,23 +4,23 @@ from event_bus.bus import EventBus
 
 # Constants
 bus = EventBus()
-TEST_VAR = 'Init'
-EVENT_NAME = 'emit_test'
+GLOBAL_VAR = 'Init'
+EVENT_NAME = 'completed'
 
 
 @bus.on(event=EVENT_NAME)
 def subscription():
-    global TEST_VAR
-    TEST_VAR = 'Finished'
+    global GLOBAL_VAR
+    GLOBAL_VAR = 'Finished'
 
 
 def test_event_emit():
     """ Tests that a function subscribed to an event executes on emit. """
 
     # Before Emit
-    assert TEST_VAR == 'Init'
+    assert GLOBAL_VAR == 'Init'
 
     bus.emit(EVENT_NAME)
 
     # After Emit
-    assert TEST_VAR == 'Finished'
+    assert GLOBAL_VAR == 'Finished'
