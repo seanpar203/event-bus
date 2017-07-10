53e77dfa14c1bfb1add4f6fac1672fd55ce46d80
Sean Parsons <seanpatrick2013@gmail.com>
2017-07-10 15:25:43 -0400

    Sync version, tag.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
 	name='event-bus',
-	version='1.0.1',
+	version='1.0.2',
 	packages=['event_bus'],
 	url='https://github.com/seanpar203/event-bus',
 	license='MIT',
 	author='Sean Parsons',
 	author_email='seanpatrick2013@gmail.com',
 	description='A simple python event bus.',
-	download_url='https://github.com/seanpar203/event-bus/archive/1.0.1.tar.gz',
+	download_url='https://github.com/seanpar203/event-bus/archive/1.0.2.tar.gz',
 )
